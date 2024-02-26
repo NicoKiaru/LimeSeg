8288e2e53ee449b93991ab02c942be1f8e1ff8d6
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-02-26 16:42:40 +0100

    Updates parent pom and fix versions Adds a simple way to run ImageJ in tests

---
+++ src/test/java/SimpleIJLaunch.java
@@ -0,0 +1,10 @@
+import net.imagej.ImageJ;
+
+public class SimpleIJLaunch {
+
+    public static void main(String... args) {
+        ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+    }
+
+}
